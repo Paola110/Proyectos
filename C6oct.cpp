ce950b67fd21dc2684bbea1bbc1f29c09f5e2c08
Paola <140652897+Paola110@users.noreply.github.com>
2023-10-07 02:07:17 +0000

    Update 3 files

---
+++ C6oct.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h> 
+using namespace std;
+#define SPEED ios_base::sync_with_stdio(0);
+
+int main() {
+    SPEED; 
+    int casas[4], dir;
+    for (int i=0; i<4; i++){
+        cin>>dir;
+        casas[i]=dir;
+    }
+    sort (casas.being(), casas.end());
+    
+	return 0;
+}
