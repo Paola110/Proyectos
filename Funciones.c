fe7d40be889cd46e0b1fdef5e2e7926094e9a81d
Paola <140652897+Paola110@users.noreply.github.com>
2023-10-18 18:08:11 +0000

    Case 1

--- Funciones.c
+++ Funciones.c
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <locale.h>
 
+void sum (float a,float b);
 
 int main (){
 
@@ -16,42 +17,53 @@ int main (){
     int op=0;
     float n1=0, n2=0;
 
+    do {
     printf("\n\tMENU\n1.Suma\n2.Resta\n3.Multiplicación\n4.División\n5.Potencia\n6.Factorial\n7.Salir\n\nElige una opcion: ");
  
-    do{
-      scanf("%i", &op);
-      if (op<1 || op>7){
-        printf("\nError: debe ser una opcion entre 1 y 7\n\nElige una opcion: ");
-      }
-      if (op>0 && op<6){
-        printf("\n\nDame el primer numero: ");
-        scanf("%f",&n1);
-        printf("\n\nDame el segundo numero: ");
-        scanf("%f",&n2);
-      } else if (op==6){
-        printf("\n\nDame el numero a factrizar: ");
-        scanf("%f",&n1);
-      }
-    }while(op<1 || op>7);
- 
-    switch (op){
-      case 1:
-        break;
-      case 2:
-        break;
-      case 3:
-        break;
-      case 4:
-        break;
-      case 5:
-        break;
-      case 6:
-        break;
-      case 7:
-        printf("Salir");
-        exit(0);
-      default:
-        break;
-    }  
+     do{
+        scanf("%i", &op);
+        if (op<1 || op>7){
+          printf("\nError: debe ser una opcion entre 1 y 7\n\nElige una opcion: ");
+        }
+        if (op>0 && op<6){
+          printf("\n\nDame el primer numero: ");
+          scanf("%f",&n1);
+          printf("\n\nDame el segundo numero: ");
+          scanf("%f",&n2);
+        } else if (op==6){
+          printf("\n\nDame el numero a factrizar: ");
+          scanf("%f",&n1);
+        }
+      }while(op<1 || op>7);
+      
+      switch (op){
+        case 1:
+          sum(n1,n2);
+          printf("\n");
+          break;
+        case 2:
+          break;
+        case 3:
+          break;
+        case 4:
+          break;
+        case 5:
+          break;
+        case 6:
+          break;
+        case 7:
+          printf("Salir");
+          break;
+        default:
+          break;
+      }  
+
+    } while (op!=7); 
+
   return 0;
+}
+
+void sum(float a, float b)
+{
+  printf("\nLa suma de ambos numeros es %f", a+b);
 }
