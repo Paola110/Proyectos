f930e528b4ff7461165e587545d5d1b76677c805
Paola <140652897+Paola110@users.noreply.github.com>
2023-11-08 03:37:35 +0000

    MMatries terminado

--- MMatriz.cpp
+++ MMatriz.cpp
@@ -14,11 +14,17 @@ int main() {
     srand (time (NULL));
 
     do{
-        printf("\n\Bienvenido al menu: \n\n");
+        printf("\n\nBienvenido al menu: \n\n");
         printf("\n Proporciona la dimension de la matriz A, minimo 2 máximo 10, fila y columna, separados por comas \n");
         scanf ("%d,%d",&filA, &columA);
         printf("\n Proporciona la dimension de la matriz B, minimo 2 máximo 10, fila y columna, separados por comas \n");
         scanf ("%d,%d",&filB,&columB);
+        if (columA!=filB){
+            printf("\nNo se puede hacer la multiplicacion si el numero de filas de la matriz A\nes diferente del numero de columnas de la matriz B, ingresa nuevamente los datos.");
+        }
+        if (columA<2||columA>10||columB<2||columB>10||filA<2||filA>10){
+            printf("\nLas dimenciones deben ser de minimo 2 y maximo 10\n\nProporciona los datos nuevamente\n");
+        }
     } while (columA<2||columA>10||columB<2||columB>10||filA<2||filA>10||columA!=filB);
     
     printf ("\n\n Matriz A\n\n");
@@ -31,19 +37,19 @@ int main() {
         }
     }
 
-    printf ("In\n Matriz B\n\n");
+    printf ("\n\n Matriz B\n\n");
 
     for(i=0;i<filB;i++){
         printf ("\n");
         for(j=0;j<columB;j++){
-            B[i][j]=rand()*9+1;
+            B[i][j]=rand()%9+1;
             printf("%d\t",B[i][j]);
         }
     }
 
     //Multiplicar Matrices
 
-    printf("\n\nMatriz resultante");
+    printf("\n\nMatriz resultante\n\n");
 
     for(i=0;i<filA;i++){
         for(j=0;j<columB;j++){
