64e232e5c2caf6b2f74d8b6425e05df6651f020c
Paola <140652897+Paola110@users.noreply.github.com>
2023-11-10 02:19:19 +0000

    FV 2

--- FuncionesVectores.c
+++ FuncionesVectores.c
@@ -2,6 +2,21 @@
 #include <locale.h>
 
 
+void creavector(int arr[]);
+void mymn(int arr[]);
+
+int main(){
+	
+    setlocale(LC_ALL, "spanish");
+		
+	int arr[5], arr2[5], i=0, men=0, may=0, maymen[2];
+	
+	printf("\nBienvenido, entremos a la funcion crear vector\n");
+	creavector(arr);
+	mymn(arr);
+	return 0;
+}
+
 void creavector(int arr[]){
 	int i;
 
@@ -12,19 +27,16 @@ void creavector(int arr[]){
 	return;
 }
 
+void mymn(int arr[]){
+	int i, may, men;
 
-int main(){
-	
-    setlocale(LC_ALL, "spanish");
-		
-	int arr[5], arr2[5], i=0, men=0, may=0, maymen[2];
-	
-	printf("\nBienvenido, entremos a la funcion crear vector\n");
-	creavector(arr);
-	
-	return 0;
+	men=arr[0];
+	may=arr[0];
+
+	for (i=0; i<5; i++){
+		if (arr[i]<men) ? men=arr[i];
+		if (arr[i]>may) ? may=arr[i];
+	}
+
+	printf("De estos números, el menor es %d y el mayor es %d", men, may);
 }
- 
-
-
-
