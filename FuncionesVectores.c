b0c2a7cb20a3503ce702606096a5bce616c0a07d
Paola <140652897+Paola110@users.noreply.github.com>
2023-11-10 02:07:25 +0000

    FV

--- FuncionesVectores.c
+++ FuncionesVectores.c
@@ -12,45 +12,16 @@ void creavector(int arr[]){
 	return;
 }
 
-void mayormenor(int arr2[]){
-	int i, men=0, may=0;
-
-	for (i=0; i<5; i++){
-		printf("%d  ", arr2[i]);
-	}
-
-	printf("\nBuscaremos número mayor y menor del vector\n");
-	
-    for (i=0; i<5; i++){
-		if (i=0){
-			men=arr2[i];
-			may=arr2[i];
-		}
-		if (men<arr2[i]&& i=!0){
-			men=arr2[i];
-		}
-		if (may>arr2[i]&& i=!0){
-			may=arr2[i];
-		}
-	}
-	return;
-}
 
 int main(){
 	
     setlocale(LC_ALL, "spanish");
 		
-	int arr[5], arr2[5], i=0, men=0, may=0;
+	int arr[5], arr2[5], i=0, men=0, may=0, maymen[2];
 	
 	printf("\nBienvenido, entremos a la funcion crear vector\n");
 	creavector(arr);
 	
-    for (i=0; i<5; i++){
-        arr2[i]=arr[i];
-    }
-
-	printf("\nAhora entremos a la funcion mayor menor\n");
-	mayormenor(arr2);
 	return 0;
 }
  
