bc75ea29be4e37eee0f857222fc70e3e72873cf9
Paola <140652897+Paola110@users.noreply.github.com>
2023-11-10 03:17:19 +0000

    FV terminada

--- FuncionesVectores.c
+++ FuncionesVectores.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
-#include <locale.h>
 
 
 void creavector(int arr[]);
 void mymn(int arr[]);
 
-int main(){
-	
-    setlocale(LC_ALL, "spanish");
+int main(){	
 		
 	int arr[5], arr2[5], i=0, men=0, may=0, maymen[2];
 	
-	printf("\nBienvenido, entremos a la funcion crear vector\n");
+	printf("\nBienvenido, entremos a la funci%cn crear vector\n", 162);
 	creavector(arr);
+	printf("\nSu vector es\n\n");
+	for (i=0; i<5; i++){
+		printf(" %d ", arr[i]);
+	}
+	printf("\n");
+	printf("\nAhora entremos a la funci%cn para buscar el mayor y el menor de los valores\n", 162);
 	mymn(arr);
 	return 0;
 }
@@ -20,7 +23,7 @@ int main(){
 void creavector(int arr[]){
 	int i;
 
-	printf("\nIngrese los 5 números del vector separados por enter o espacios\n");
+	printf("\nIngrese los 5 n%cmeros del vector separados por enter o espacios\n", 163);
 	for (i=0; i<5; i++){
 		scanf("%d", &arr[i]);
 	}
@@ -34,9 +37,9 @@ void mymn(int arr[]){
 	may=arr[0];
 
 	for (i=0; i<5; i++){
-		arr[i]<men ? men=arr[i];
-		arr[i]>may ? may=arr[i];
+		arr[i]<men ? men=arr[i] : men;
+		arr[i]>may ? may=arr[i] : may;
 	}
 
-	printf("De estos números, el menor es %d y el mayor es %d", men, may);
+	printf("\nDe estos n%cmeros, el menor es %d y el mayor es %d\n",163, men, may);
 }
